a651839e5a389b0888bc88f9eaf3dc3be1973000
Filip Florek <florson467@icloud.com>
2022-02-11 20:38:37 +0100

    added dropdown menu

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -3,19 +3,20 @@ const TopBar = ({topbarid}) => {
     return <div id={topbarid}>
         <div id='companyName'>&lt; nazwafirmy &gt;</div>
         <div id='topbarside'>
-            <div id='dropdown'>Oferta</div>
-            <div className='dropdown_content'>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
-                <a href="#"></a>
+            <div id='dropdown'>Oferta
+                <div className='dropdown_content'>
+                    <a href="#">Usługa 1</a>
+                    <a href="#">Usługa 2</a>
+                    <a href="#">Usługa 3</a>
+                    <a href="#">Usługa 4</a>
+                    <a href="#">Usługa 5</a>
+                    <a href="#">Usługa 6</a>
+                    <a href="#">Usługa 7</a>
+                    <a href="#">Usługa 8</a>
+                    <a href="#">Usługa 9</a>
+                </div>
             </div>
-            <a>Kontakt</a>
+            <a id='contact_link' href='#'>Kontakt</a>
         </div>
     </div>
 }
